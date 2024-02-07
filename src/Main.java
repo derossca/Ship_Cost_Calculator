7ff5b7704c0700150614c190b6252023f50e733f
Corey DeRossett <cderossett3@gmail.com>
2024-02-07 09:44:06 -0500

    Added pseudo code, imported scanner, declared variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+//importing scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //class ShippingCosts
+        //	main()
+        //		num itemPrice
+        //		num shippingCost
+        //		output “What is the cost of the item?”
+        //		input itemPrice
+        //		if itemPrice >= 100 then
+        //			shippingCost = 0
+        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + 					   shippingCost + itemPrice
+        //		else
+        //			shippingCost = 0.02 * itemPrice
+        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + 					   shippingCost + itemPrice
+        //		end if
+        //	return
+        //end class
+
+        //calling in scanner
+        Scanner in = new Scanner(System.in);
+        //declaring variables
+        double itemPrice = 0;
+        double shippingCost = 0;
+        //output asking for itemPrice
+        System.out.println("What is the cost of the item?");
+        //input for itemPrice
+        in.nextDouble();
+        //if statements
     }
 }
