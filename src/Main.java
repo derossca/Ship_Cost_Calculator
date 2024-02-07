0e3826419771330d1c97038071c7ed879e8b89c1
Corey DeRossett <cderossett3@gmail.com>
2024-02-07 10:34:56 -0500

    added user input for itemPrice, added if else statements and output for each

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,10 @@ public class Main {
         //		input itemPrice
         //		if itemPrice >= 100 then
         //			shippingCost = 0
-        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + 					   shippingCost + itemPrice
+        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + shippingCost + itemPrice
         //		else
         //			shippingCost = 0.02 * itemPrice
-        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + 					   shippingCost + itemPrice
+        //			output “Shipping is “ + shippingCost + “. “ + “Your total is “ + shippingCost + itemPrice
         //		end if
         //	return
         //end class
@@ -25,10 +25,20 @@ public class Main {
         //declaring variables
         double itemPrice = 0;
         double shippingCost = 0;
+        double totalCost = itemPrice + shippingCost;
         //output asking for itemPrice
         System.out.println("What is the cost of the item?");
-        //input for itemPrice
-        in.nextDouble();
-        //if statements
+        //input itemPrice
+        itemPrice = in.nextDouble();
+        //if statements with input of itemPrice and output shipping cost and total cost
+        if(itemPrice >= 100){
+            shippingCost = 0;
+            System.out.println("Shipping is " + shippingCost + " and your total is " + (shippingCost + itemPrice));
+        }
+        //else shipping is 2% of itemPrice and output shipping and total cost
+        else {
+            shippingCost = .02 * itemPrice;
+            System.out.println("Shipping is " + shippingCost + " and your total is " + (shippingCost + itemPrice));
+        }
     }
 }
